024140b05433810b8c0b92edbe1980501743e09e
hiramson normil <hiramsonnormil00@gmail.com>
2026-08-11 22:36:46 -0300

    initialize project with Playwright setup and configuration

---
+++ playwright.config.js
@@ -0,0 +1,43 @@
+// ...existing code...
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  // Execução
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
+  // Falha o CI caso exista test.only
+  forbidOnly: !!process.env.CI,
+
+  // Relatório
+  reporter: 'html',
+
+  // Configuração padrão dos testes
+  use: {
+    baseURL: 'https://www.automationexercise.com',
+    trace: 'on-first-retry',
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  // Navegadores
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
