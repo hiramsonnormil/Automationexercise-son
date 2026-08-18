b4570f4b37f00d217fc529d4406852b7bfc5f1d9
hiramson normil <hiramsonnormil00@gmail.com>
2026-08-18 19:09:50 -0300

    add test case for incorrect credential login functionality

---
+++ helpers/accounts/test-case/cases_acounts.js
@@ -0,0 +1,46 @@
+import { expect } from "playwright/test";
+
+/** steps
+ * 
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Signup / Login' button
+5. Verify 'Login to your account' is visible
+6. Enter incorrect email address and password
+7. Click 'login' button
+8. Verify error 'Your email or password is incorrect!' is visible
+ */
+// case 1 incorrect credential login
+
+async function incorrectCredentialCase(page) {
+  const locators = (page) => {
+        return {
+            linkLogin: page.getByRole('link', { name: 'Login' }),
+            input_email: page.locator('[data-qa="login-email"]'),
+            input_password: page.locator('[data-qa="login-password"]'),
+        };
+    };
+
+    const locator = locators(page)
+    await locator.linkLogin.click()
+    try {
+        await expect(page.getByText("Login to your account")).toBeVisible()
+        await locator.input_email.type(process.env.EMAIL)
+        await locator.input_password.type(process.env.INCORECTPASSWORD)
+    } catch (error) {
+        throw new Error("for some reason the login text is not visible" + error)
+    }
+
+    await page.locator('[data-qa="login-button"]').click()
+
+    await expect(page.getByText("Your email or password is incorrect!")).toBeVisible()
+    console.log("case pass")
+}
+
+
+
+
+export{
+    incorrectCredentialCase
+}
--- tests/Login/login.spec.js
+++ tests/Login/login.spec.js
@@ -2,6 +2,7 @@ import test from "playwright/test";
 import { SetupBeforeach } from "../../utils/beforeEach/beforeEach";
 import { signup } from "../../helpers/accounts/signup";
 import { Login } from "../../helpers/accounts/login";
+import { incorrectCredentialCase } from "../../helpers/accounts/test-case/cases_acounts";
 
 SetupBeforeach()
 
@@ -13,3 +14,6 @@ test("login", async({page})=>{
     await Login(page)
 })
 
+test("case 002, incorrect credential login", async({page})=>{
+    await incorrectCredentialCase(page)
+})
